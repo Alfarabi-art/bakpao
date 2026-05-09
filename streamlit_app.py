23778256233d87b4fa0e459ed0a18acc36a03e06
Alfarabi-art <muhammadalfarabi992@gmail.com>
2026-05-10 00:12:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,31 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+st.success("Google Auth berhasil")
+
 try:
 
-    scope = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
-
-    creds = Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=scope
+    spreadsheet = client.open_by_url(
+        "https://docs.google.com/spreadsheets/d/157PtqTU5MmkF5Zf1KiPPsIBwvomXOoyVieifrYaz6M4/edit#gid=0"
     )
 
-    st.success("Secrets berhasil dibaca")
+    st.success("Spreadsheet berhasil dibuka")
 
-    client = gspread.authorize(creds)
+    worksheet = spreadsheet.worksheet("Data")
 
-    st.success("Google Auth berhasil")
+    st.success("Worksheet Data berhasil dibuka")
 
 except Exception as e:
 
